573a91a137ab21bc559267ec6d22041a3aa3f905
Mikita.Shah <mikitashah2009@gmail.com>
2024-10-31 14:39:06 +0530

    add an interface which type checks the JSON response

---
+++ src/types/CryptoData.ts
@@ -0,0 +1,9 @@
+export interface CryptoData {
+  bitcoin: { usd: number };
+  ethereum: { usd: number };
+  dogecoin: { usd: number };
+}
+
+// this interfaxe ensure typescript will expect each cryptocurreny to have a USD price.
+
+
