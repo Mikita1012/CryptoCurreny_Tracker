a4bde236d5b3e9d151215c1aa6a9f187690f5035
Mikita.Shah <mikitashah2009@gmail.com>
2024-10-31 15:04:58 +0530

    App.tsx, we’ll fetch the cryptocurrency data from the API, store it in state, and pass the data to CryptoCard components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { CryptoData } from './types/CryptoData';
+import CryptoCard from './components/CryptoCard';
+import axios from 'axios';
 
-function App() {
+
+
+const App: React.FC = () => {
+  const [cryptoData, setCryptoData] =  useState<CryptoData | null>(null)
+  const [error, setError] = useState<string | null>(null);
+
+
+  useEffect(() => {
+    const fetchCryptoData = async () => {
+      try {
+        const response = await axios.get<CryptoData>(
+          'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,dogecoin&vs_currencies=usd'
+        );
+        setCryptoData(response.data)
+      } catch (err) {
+        setError("Failded to fetch cryptocurrency data. ")
+      }
+    };
+    fetchCryptoData();
+  }, [])
+
   return (
-    <></>
+    <div className='App'>
+      <h1>Cryptocurreny Price Tracker</h1>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <div className="crypto-container">
+        {crypto ? (
+          <>
+            <CryptoCard name="Bitcoin" price={cryptoData?.bitcoin?.usd ?? 0} />
+            <CryptoCard name="Ethereum" price={cryptoData?.ethereum?.usd ?? 0} />
+            <CryptoCard name="Dogecoin" price={cryptoData?.dogecoin?.usd ?? 0 } />
+          </>
+        ) : (
+          <p>Loading ...</p>
+        )}
+      </div>
+    </div>
   );
 }
 
